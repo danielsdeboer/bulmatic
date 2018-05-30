83e9d3f39dc5ca122bdb1f743a3626b2509f994b
Daniel S. Deboer <daniel.s.deboer@gmail.com>
2018-05-30 16:11:13 -0400

    Update entry

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import Columns from './components/columns/Columns.vue'
 import Column from './components/columns/Column.vue'
 
 module.exports = {
-  components: {
-    Columns,
-    Column,
-  },
+  Columns,
+  Column,
 }
