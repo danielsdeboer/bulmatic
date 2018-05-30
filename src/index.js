5db745fef985d1e691739e1dfa1a8ec1ff51cea3
Daniel S. Deboer <daniel.s.deboer@gmail.com>
2018-05-30 16:07:17 -0400

    Add index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Columns from './components/columns/Columns.vue'
+import Column from './components/columns/Column.vue'
+
+module.exports = {
+  components: {
+    Columns,
+    Column,
+  },
+}
