b0afb53f767601b64c8c34f6410a0f5eb2293b77
Daniel S. Deboer <daniel.s.deboer@gmail.com>
2018-05-31 09:51:06 -0400

    Export container

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import Columns from './components/columns/Columns.vue'
 import Column from './components/columns/Column.vue'
+import Container from './components/layout/Container.vue'
 
 export {
   Columns,
-  Column
+  Column,
+  Container
 }
