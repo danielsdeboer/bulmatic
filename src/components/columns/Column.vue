6775788d8066660f04c8414a86c3cd5271239f06
Daniel S. Deboer <daniel.s.deboer@gmail.com>
2018-05-30 20:53:03 -0400

    Add eslint deps

--- src/components/columns/Column.vue
+++ src/components/columns/Column.vue
@@ -34,7 +34,7 @@
           this.sizeClass,
           this.offsetClass,
         ]
-      },
+      }
     },
   }
 </script>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import Columns from './components/columns/Columns.vue'
 import Column from './components/columns/Column.vue'
 
-export const Columns
-export const Column
-
-export default {
-  columns: {
-    Columns,
-    Column,
-  },
+export {
+  Columns,
+  Column
 }
