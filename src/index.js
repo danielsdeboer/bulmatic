be51b7ab937c2d1295e0a0b8db38c3ae93dc1abe
Daniel S. Deboer <daniel.s.deboer@gmail.com>
2018-05-30 20:40:15 -0400

    Add non-default module exports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import Columns from './components/columns/Columns.vue'
 import Column from './components/columns/Column.vue'
 
+export const Columns
+export const Column
+
 export default {
-  Columns,
-  Column,
+  columns: {
+    Columns,
+    Column,
+  },
 }
